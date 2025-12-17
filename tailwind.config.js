72478290cc92b77ac21f334b0bcf2b8fce817f8e
DevOps Administrator <159426907+KaarAdmin@users.noreply.github.com>
2025-12-17 19:43:50 +0000

    updated brand

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
 	darkMode: false, // or 'media' or 'class'
 	theme: {
                fontFamily: {
-                       body: ['Geist', 'Overpass', 'sans-serif'],
+                       body: ['Geist', 'sans-serif'],
                },
 		extend: {
 			screens: {
